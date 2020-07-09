a5416846f8cc2b4b6223052804c28ee736419d62
Roland Groen <roland@edia.nl>
2020-07-09 12:19:32 +0200

    Adding license headers

--- src/main/java/nl/gidsopenstandaarden/ri/portal/configuration/HtiConfiguration.java
+++ src/main/java/nl/gidsopenstandaarden/ri/portal/configuration/HtiConfiguration.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2020 Headease B.V., This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
+ */
+
 package nl.gidsopenstandaarden.ri.portal.configuration;
 
 import nl.gidsopenstandaarden.ri.portal.util.KeyUtils;
